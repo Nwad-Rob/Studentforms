47b4f35033c880d600cce6a9a3e26b10056a6109
Nawd-Rob <rnwadinobi@gmail.com>
2023-10-02 17:58:38 -0500

    Created Button for adding data into the table

--- thymeleaf/src/main/java/com/students/thymeleaf/Controller/StudentController.java
+++ thymeleaf/src/main/java/com/students/thymeleaf/Controller/StudentController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.students.thymeleaf.Model.Student;
 import com.students.thymeleaf.Repository.StudentRepository;
 
 @Controller
-
 public class StudentController {
     @Autowired
     private StudentRepository studentRepo;
@@ -22,5 +23,20 @@ public class StudentController {
         List<Student> studentList = studentRepo.findAll();
         mav.addObject("students", studentList);
         return mav;
+    }
+
+    @GetMapping("/addStudentForm")
+    public ModelAndView addStudentForm(){
+       ModelAndView mav = new ModelAndView("add-student-form");
+       Student newStudent = new Student();
+       mav.addObject("student", newStudent);
+       return mav;
+    }
+
+
+    @PostMapping("/saveStudent")
+    public String saveStudent (@ModelAttribute Student student){
+        studentRepo.save(student);
+        return "redirect:/showStudents";
     }
 }
--- thymeleaf/src/main/java/com/students/thymeleaf/Repository/StudentRepository.java
+++ thymeleaf/src/main/java/com/students/thymeleaf/Repository/StudentRepository.java
@@ -1,14 +1,12 @@
 package com.students.thymeleaf.Repository;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.students.thymeleaf.Model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    public List<Student> findByName(String stuentname);
-    public List<Student> findByEmail(String email);
-    public List<Student> findByDepartment(String department);
+  
     
 }
